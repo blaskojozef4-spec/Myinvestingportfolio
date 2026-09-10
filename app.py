9bc2ee0f0d87779a9a9eb3c93f16388d1d783977
blaskojozef4-spec <blaskojozef4@gmail.com>
2026-09-10 07:43:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ portfolio = [
     {'ticker': 'CSU.TO',   'pocet': 0.3048237,     'priemerna_cena': 3025.75,  'mena': 'CAD'},
 ]
 
-CLOSED_PROFIT_USD = 327.06
+CLOSED_PROFIT_USD = 1974.17
 TOTAL_DIVIDENDS_USD = 13384.75
 
 
