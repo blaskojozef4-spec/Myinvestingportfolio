baff0d3cb4ddf2355effdde8a31835722a4a1683
blaskojozef4-spec <blaskojozef4@gmail.com>
2026-09-11 17:06:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 portfolio = [
     {'ticker': 'IWM',      'pocet': 32.10317,      'priemerna_cena': 202.48,   'mena': 'USD'},
     {'ticker': 'XLP',      'pocet': 55.87663,      'priemerna_cena': 76.29,    'mena': 'USD'},
-    {'ticker': 'IVV',      'pocet': 65.79541,      'priemerna_cena': 581.74,   'mena': 'USD'},
+    {'ticker': 'IVV',      'pocet': 83.79687,      'priemerna_cena': 621.84,   'mena': 'USD'},
     {'ticker': 'IEMG',     'pocet': 120.6516,      'priemerna_cena': 55.79,    'mena': 'USD'},
     {'ticker': 'CNDX.L',   'pocet': 2.99,          'priemerna_cena': 1231.72,  'mena': 'USD'},
     {'ticker': 'DTLA.L',   'pocet': 8275.63052713, 'priemerna_cena': 4.4404,   'mena': 'USD'},
